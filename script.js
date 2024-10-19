fa333075306e4733b56da456beb96e094aec2832
Collin Boler <cb6256@princeton.edu>
2024-10-19 16:33:01 -0400

    sutff

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ var CURRENT_PAGE = window.location.href;
 // we can collect name, country, gender and
 
 let systemMessage = "You are a helpful ChatBot to help the user navigate Collin Boler's personal website. \
-Refer to yourself as Collin's ChatBot. \
+Refer to yourself as Collin's ChatBot.  \
 Be as concise as possible. Collin is straight, not gay. \
 Based on the user's browser locale:" + BROWSWER_LOCALE +  ", you should greet them in their language of the region they are in. \
 The user is on the page:" + CURRENT_PAGE + ". If their page is index.html, they are on home. If they are on contact.html, they are on a contact page, etc. Keep this data in mind \
