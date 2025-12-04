29fc6749fd82e998ba7148112f4fca706428f2f8
collinboler <155112701+collinboler@users.noreply.github.com>
2025-12-04 02:06:06 -0500

    take 1

--- config.js
+++
@@ -1,7 +0,0 @@
-// This file should be added to .gitignore to keep API keys secure
-// DO NOT commit this file to GitHub or any public repository
-
-// OpenAI API Key
-const OPENAI_API_KEY = "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";
-
-// You can add other configuration settings here 
